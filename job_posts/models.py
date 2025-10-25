aaeb3f4cca6523fe8d2ef2deb62d5d80aaf3000e
Senthamil-Selvan-17 <senthamil1703@gmail.com>
2025-10-25 19:01:54 +0530

    initial push

---
+++ job_posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    company = models.CharField(max_length=500)
+    title = models.TextField()
+    com_description = models.TextField()
+    job_details = models.JSONField(default=dict, blank=True)
+    job_description = models.TextField()
+    link = models.URLField()
+
+    def __str__(self):
+        return (f"{self.company} : {self.title}")
