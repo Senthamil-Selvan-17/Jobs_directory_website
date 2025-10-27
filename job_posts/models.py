60622f944cbb1c65b3c7db360e19211f1b4458cc
Senthamil-Selvan-17 <senthamil1703@gmail.com>
2025-10-27 23:09:36 +0530

    new feat

--- job_posts/models.py
+++ job_posts/models.py
@@ -8,6 +8,7 @@ class Job(models.Model):
     job_details = models.JSONField(default=dict, blank=True)
     job_description = models.TextField()
     link = models.URLField()
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return (f"{self.company} : {self.title}")
--- job_posts/views.py
+++ job_posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+def index(request):
+    jobs = Job.objects.all()
+    return render(request, 'job_posts/index.html', {
+        'jobs':jobs
+    })
+
+def job(request, title):
+    job = Job.objects.get(title = title)
+    return render(request, 'job_posts/job.html',{
+        'job': job
+    })
