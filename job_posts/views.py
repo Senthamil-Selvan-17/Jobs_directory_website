8eecd23024dc9807e9ef308ee91b585c6db05d18
Senthamil-Selvan-17 <senthamil1703@gmail.com>
2025-11-04 12:39:56 +0530

    added feat

--- job_posts/views.py
+++ job_posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Job
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your views here.
 def index(request):
-    jobs = Job.objects.all()
+    one_week_data = timezone.now().date() - timedelta(days=7)  # it gives the date before seven days which used in filter to give data after that date
+    jobs = Job.objects.all().filter(date__gte = one_week_data).order_by('-date') #it gives data in descending,  gte is lookup which means greater than or equal to  
     return render(request, 'job_posts/index.html', {
         'jobs':jobs
     })
